4e61653bc49ff5106e9cd021be5f3db8355d5032
Antoni Tito <antonitm96@gmail.com>
2024-04-17 11:16:40 -0500

    Reactive Forms: Editar Formulario

--- src/app/agregar/agregar.component.ts
+++ src/app/agregar/agregar.component.ts
@@ -14,6 +14,10 @@ interface Usuario{
 })
 export class AgregarComponent implements OnInit {
   formularioCreado: FormGroup;
+  esNuevo:boolean = true;
+  posicionRel:number = -1;
+
+
   usuarios: Array<Usuario>  = new Array<Usuario>();
   constructor(private formBuilder: FormBuilder) { }
 
@@ -38,4 +42,42 @@ export class AgregarComponent implements OnInit {
     this.formularioCreado.reset();
     //console.log(this.formularioCreado.value);
   }
+
+  
+
+  editarUsuario(posicion: number){
+    
+    //this.formularioCreado.patchValue({ nombre: this.usuarios[posicion].nombre });
+    //this.formularioCreado.patchValue({ correo: this.usuarios[posicion].correo });
+    //this.formularioCreado.patchValue({ password: this.usuarios[posicion].password });
+
+    this.formularioCreado.setValue({
+      nombre:   this.usuarios[posicion].nombre,
+      correo:   this.usuarios[posicion].correo,
+      password: this.usuarios[posicion].password
+    })
+
+
+    this.esNuevo = false;
+    this.posicionRel = posicion;
+    //this.usuarios[posicion].nombre = 'Anto';
+    //this.usuarios[posicion].correo = 'Anto@gmail.com';
+    //this.usuarios[posicion].password = '12345678';
+    console.log(this.formularioCreado.controls);
+
+  }
+
+
+  editar(){
+    this.usuarios[this.posicionRel].nombre = this.formularioCreado.value.nombre;
+    this.usuarios[this.posicionRel].correo = this.formularioCreado.value.correo;
+    this.usuarios[this.posicionRel].password = this.formularioCreado.value.password;
+    this.formularioCreado.reset();
+    this.esNuevo = true;
+    this.posicionRel = -1;
+  }
+
+
+
+
 }
